44e69400d7f26dadb5011ae23b0062037abcc613
Magnus Christian Larsen <macl@itconfidence.dk>
2022-11-09 23:02:00 +0100

    Removed unused dependency

--- src/server.ts
+++ src/server.ts
@@ -28,19 +28,17 @@ io.on('connect', (socket: Socket) => {
     console.log('Client disconnected from socket')
   })
   socket.on('play', () => {
-    bucket.openDownloadStreamByName('all_my_love.mp3').on('data', (chunk) => {
-      console.log(chunk)
-      socket.emit('audio-chunk', chunk)
-    })
+    bucket.openDownloadStreamByName('all_my_love.mp3')
+      .on('data', (chunk) => {
+        console.log(chunk)
+        socket.emit('audio-chunk', chunk)
+      })
   })
 })
 
 app.get('/play', (req, res) => {
   bucket.openDownloadStreamByName('all_my_love.mp3').pipe(res)
 })
-
-
-
 
 server.listen(802, () => {
   // Song.create({ data: '21312321fsdgdfshgdfs' }).save()
