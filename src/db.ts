ac2b12a7b4bda5f4fc25a0bd9a06e91b9e458a47
Magnus Christian Larsen <macl@itconfidence.dk>
2022-11-10 00:00:37 +0100

    Added env loading and removed db file

--- src/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { connect } from 'mongoose'
-
-// Connects to database and exports the database model
-export const connectDB = async () => {
-  mongoose.connect('mongodb://localhost:27017').catch((err) => {
-    console.error(err)
-  })
-}
