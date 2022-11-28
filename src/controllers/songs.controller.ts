69602dc55e3a9b614de7f6e87eb4a541c29384b0
Magnus Christian Larsen <macl@itconfidence.dk>
2022-11-28 21:50:07 +0100

    Added mimetype header

--- src/controllers/songs.controller.ts
+++ src/controllers/songs.controller.ts
@@ -2,22 +2,27 @@ import { Request, Response } from 'express'
 import { bucket } from '../database'
 import fs from 'fs'
 
-
 export function getSongByID(req: Request, res: Response) {
-    bucket
-        .openDownloadStreamByName(req.params.songId)
-        .on('error', () => {
-            res.sendStatus(400)
-        })
-        .pipe(res)
+  res.set('Content-Type', 'audio/mpeg')
+  //   res.set(
+  //     'Content-Disposition',
+  //     'attachment; filename="' + req.file.filename + '"'
+  //   )
+  bucket
+    .openDownloadStreamByName(req.params.songId)
+    .on('error', () => {
+      res.sendStatus(400)
+    })
+    .pipe(res)
 }
 
 export function createSong(req: Request, res: Response) {
-    console.log(req.file)
-    if (!req.file) return
-    fs.createReadStream(req.file.path).pipe(
-        bucket.openUploadStream(req.body.id).on('finish', () => {
-            res.sendStatus(202)
-        })
-    )
+  console.log(req.file)
+  if (!req.file) return
+
+  fs.createReadStream(req.file.path).pipe(
+    bucket.openUploadStream(req.body.id).on('finish', () => {
+      res.sendStatus(202)
+    })
+  )
 }
